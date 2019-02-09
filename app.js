3a095b62ae5b1b15fd0fdcbdb94c9e250eb12432
Jaleed Ur Rehman <jaleed12@me.com>
2019-02-08 17:52:31 -0800

    added webDetect function

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
-async function quickstart() {
-    // Imports the Google Cloud client library
-    const vision = require('@google-cloud/vision');
-  
-    // Creates a client
-    const client = new vision.ImageAnnotatorClient();
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient();
+const fs = require('fs');
+const exec = require('child_process').exec;
+
+async function fetchLabels(imageUri) {
   
     // Performs label detection on the image file
-    const [result] = await client.labelDetection('./food-images/rotten-apple.jpg');
+    const [result] = await client.labelDetection(imageUri);
+
+    fs.writeFile('test.json', JSON.stringify(result), 'utf8', (err) => {
+        if (err) {
+            console.error(err, err.stack);
+            return;
+        }
+    });
+
     const labels = result.labelAnnotations;
     console.log('Labels:');
     labels.forEach(label => console.log(label.description));
 }
 
-quickstart();
+async function webDetect(imageUri) {
+    const [result] = await client.webDetection(imageUri);
+
+    fs.writeFile('test.json', JSON.stringify(result), 'utf8', (err) => {
+        if(err) {
+            console.error(err, err.stack);
+            return;
+        }
+    });
+
+    const imageTitle = result.webDetection.bestGuessLabels[0].label;
+    console.log(imageTitle);
+
+}
+
+webDetect('./food-images/rotten-apple.jpg');
