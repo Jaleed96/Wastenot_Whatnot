a0b735ba992c52113a7161f65326032b65b744be
Sam Tadey <stadey@my.bcit.ca>
2019-02-09 20:04:37 -0800

    Adding to Jay's updates

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
-async function quickstart() {
-    // Imports the Google Cloud client library
-    const vision = require('@google-cloud/vision');
-  
-    // Creates a client
-    const client = new vision.ImageAnnotatorClient();
-  
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient();
+const fs = require('fs');
+const exec = require('child_process').exec;
+const categories = require('./categories.json');
+
+async function fetchLabels(imageUri) {
     // Performs label detection on the image file
-    const [result] = await client.labelDetection('./food-images/rotten-apple.jpg');
+    const [result] = await client.labelDetection(imageUri);
+
+    fs.writeFile('test.json', JSON.stringify(result), 'utf8', (err) => {
+        if (err) {
+            console.error(err, err.stack);
+            return;
+        }
+    });
+
     const labels = result.labelAnnotations;
+
+    let params = [];
     console.log('Labels:');
-    labels.forEach(label => console.log(label.description));
+    labels.forEach(label => params.push(label.description));
+
+    console.log(getCategory(params));
 }
 
-quickstart();
+async function webDetect(imageUri) {
+    const [result] = await client.webDetection(imageUri);
+
+    fs.writeFile('test.json', JSON.stringify(result), 'utf8', (err) => {
+        if(err) {
+            console.error(err, err.stack);
+            return;
+        }
+    });
+
+    const imageTitle = result.webDetection.bestGuessLabels[0].label;
+    console.log(imageTitle);
+
+}
+
+function getCategory(apiResponse) {
+    console.log(apiResponse[0]);
+    console.log(categories.compost[0]);
+
+
+
+    if (apiResponse[0].toUpperCase() === categories.compost[0].toUpperCase()) {
+        return "Compost: " + apiResponse[0];
+    }
+
+    return "Couldn't find match";
+}
+
+fetchLabels('./food-images/rotten-apple.jpg');
